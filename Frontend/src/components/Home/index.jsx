223a2525d5dfb0f6440516607ae2607619a7ba66
AshwaniSingh12 <as3141538@gmail.com>
2026-01-04 18:39:03 +0530

    Home Layout

---
+++ Frontend/src/components/Home/index.jsx
@@ -0,0 +1,11 @@
+import Homelayout from "../../Layout/Homelayout";
+
+const Homepage = () => {
+  return (
+    <Homelayout>
+      <h1 className="text-5xl text-green-500">Login Page</h1>
+    </Homelayout>
+  );
+};
+
+export default Homepage;
